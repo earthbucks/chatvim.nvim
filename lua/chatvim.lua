4faa8b1b196fa06b584c92f8d777c3eec754b521
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-01 08:59:44 -0700

    hello_node

--- lua/chatvim.lua
+++ lua/chatvim.lua
@@ -1,8 +1,15 @@
 local M = {}
 
 function M.hello()
-  -- vim.api.nvim_out_write("Hello world from chatvim.nvim!\n")
-  vim.api.nvim_echo({{"Hello world from chatvim.nvim!", "Normal"}}, false, {})
+	-- vim.api.nvim_out_write("Hello world from chatvim.nvim!\n")
+	vim.api.nvim_echo({ { "Hello world from chatvim.nvim!", "Normal" } }, false, {})
+end
+
+function M.hello_node()
+	local input = "from Lua"
+	local output = vim.fn.system({ "node", "/Users/ryan/dev/chatvim.nvim/hello.js", input })
+	-- vim.api.nvim_out_write(output .. "\n")
+	vim.api.nvim_echo({ { output, "Normal" } }, false, {})
 end
 
 return M
