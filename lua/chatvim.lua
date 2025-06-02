3b286b8c071fea238a8ee1f3ea90b34e8b951bbf
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-02 08:12:23 -0700

    remove unused testing methods

--- lua/chatvim.lua
+++ lua/chatvim.lua
@@ -1,15 +1,5 @@
 local M = {}
 
-function M.hello()
-	vim.api.nvim_echo({ { "Hello world from chatvim.nvim!", "Normal" } }, false, {})
-end
-
-function M.hello_node()
-	local input = "from Lua"
-	local output = vim.fn.system({ "node", "/Users/ryan/dev/chatvim.nvim/hello.js", input })
-	vim.api.nvim_echo({ { output, "Normal" } }, false, {})
-end
-
 function M.complete_markdown()
 	local bufnr = vim.api.nvim_get_current_buf()
 if not vim.bo[bufnr].modifiable then
