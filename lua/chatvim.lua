bda3f094a6d67c780a4fe3d344968d55e59a6498
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-18 03:06:19 -0700

    remove keybindings from default

--- lua/chatvim.lua
+++ lua/chatvim.lua
@@ -474,6 +474,14 @@ vim.api.nvim_create_user_command("ChatvimWrite", chatvim_write, {
 })
 
 -- Chatvim (chatvim.nvim) keybindings
-local opts = { noremap = true, silent = true }
+-- local opts = { noremap = true, silent = true }
+-- vim.api.nvim_set_keymap("n", "<Leader>cvc", ":ChatvimComplete<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvs", ":ChatvimStop<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvw", ":ChatvimWrite<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvnn", ":ChatvimNew<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvnl", ":ChatvimNewLeft<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvnr", ":ChatvimNewRight<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvnb", ":ChatvimNewBottom<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<Leader>cvnt", ":ChatvimNewTop<CR>", opts)
 
 return M
